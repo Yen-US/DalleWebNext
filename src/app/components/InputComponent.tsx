df78e49a350ff293b739d271e3b4784bcd2d30e0
Yen-US <yen21000@gmail.com>
2023-08-18 23:35:45 -0600

    Input UI implemented

--- src/app/components/InputComponent.tsx
+++ src/app/components/InputComponent.tsx
@@ -11,7 +11,7 @@ import { Button, ButtonGroup } from "@nextui-org/button";
 
 export default function InputComponent() {
   const [value, setValue] = useState("");
-  const [selectedKeys, setSelectedKeys] = useState<Set<Key>>(new Set(["text"]));
+  const [selectedKeys, setSelectedKeys] = useState<Set<Key>>(new Set(["256px"]));
 
   const selectedValue = useMemo(
     () => Array.from(selectedKeys).join(", ").replaceAll("_", " "),
@@ -19,43 +19,57 @@ export default function InputComponent() {
   );
 
   return (
-    <div className="flex justify-center max-w-4xl mt-12">
-      <ButtonGroup>
+    <>
+      <div className="flex justify-center mt-12 items-end">
         <Textarea
           key={1}
           type="text"
           label="Prompt"
           variant="bordered"
-          description="Enter your prompt here, words separated by comma"
+          placeholder="Enter your prompt here, words separated by comma"
           onChange={(e) => setValue(e.target.value)}
           onClear={() => setValue("")}
           value={value}
-          minRows={1}
+          minRows={2}
+          className="w-4/6 mr-4"
         />
-        <Dropdown>
-          <DropdownTrigger>
-            <Button variant="bordered" className="h-28 capitalize">
-              {selectedValue}
-            </Button>
-          </DropdownTrigger>
-          <DropdownMenu
-            aria-label="Single selection actions"
-            variant="flat"
-            disallowEmptySelection
-            selectionMode="single"
-            selectedKeys={selectedKeys}
-            onSelectionChange={(e) =>
-              setSelectedKeys(e as SetStateAction<Set<Key>>)
-            }
-          >
-            <DropdownItem key="text">Text</DropdownItem>
-            <DropdownItem key="number">Number</DropdownItem>
-            <DropdownItem key="date">Date</DropdownItem>
-            <DropdownItem key="single_date">Single Date</DropdownItem>
-            <DropdownItem key="iteration">Iteration</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </ButtonGroup>
-    </div>
+      </div>
+      <div className="flex justify-center mt-4 items-end">
+        <ButtonGroup>
+          <Button className="h-14  min-w-min">Prompt Option</Button>
+          <Dropdown>
+            <DropdownTrigger>
+              <Button
+                variant="bordered"
+                className="h-14 capitalize min-w-[85px]"
+              >
+                {selectedValue}
+              </Button>
+            </DropdownTrigger>
+            <DropdownMenu
+              aria-label="Single selection actions"
+              variant="flat"
+              disallowEmptySelection
+              selectionMode="single"
+              selectedKeys={selectedKeys}
+              onSelectionChange={(e) =>
+                setSelectedKeys(e as SetStateAction<Set<Key>>)
+              }
+            >
+              <DropdownItem key="256px" value={"256x256"}>
+                256px
+              </DropdownItem>
+              <DropdownItem key="512px" value={"512x512"}>
+                512px
+              </DropdownItem>
+              <DropdownItem key="1024px" value={"1024x1024"}>
+                1024px
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+          <Button className="h-14 min-w-min">Generate</Button>
+        </ButtonGroup>
+      </div>
+    </>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import InputComponent from "./components/InputComponent"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6">
-      <div className="z-10 max-w-5xl w-full font-mono text-sm">
+      <div className="w-5/6 font-mono">
         <NavBar />
         <InputComponent/>
       </div>
